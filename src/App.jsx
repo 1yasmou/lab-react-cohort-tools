33e1ed7d8dd30fe84c4eae3ed8127963ec11ce90
Yassine Moussaif <yassine.moussaif1234@gmail.com>
2024-05-02 15:54:48 +0200

    "routingLab"

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,17 @@ import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import StudentDetailsPage from "./pages/StudentDetailsPage";
 import UserProfilePage from "./pages/UserProfilePage";
-
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-
   return (
     <div className="App relative z-20 pt-20">
       <Navbar />
-
-      <div className="pages">
-        <HomePage />
-
-        <StudentDetailsPage />
-
-        <UserProfilePage />
-      </div>
-
+      <Routes>
+        <Route path="/" Component={HomePage} />
+        <Route path="/students/:studentId" Component={StudentDetailsPage} />
+        <Route path="/profile" Component={UserProfilePage} />
+      </Routes>
     </div>
   );
 }
